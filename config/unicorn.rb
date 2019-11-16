6efdc157467626665dedaa8bb3defc35c399778c
平澤貴昭 <hirasawatakaaki@hirasawatakaakinoMacBook-Air.local>
2019-11-16 18:37:28 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -53,17 +53,6 @@ end
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
-app_path = File.expand_path('../../', __FILE__)
-
-worker_processes 1
-
-working_directory app_path
-pid "#{app_path}/tmp/pids/unicorn.pid"
-listen "#{app_path}/tmp/sockets/unicorn.sock"
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-stdout_path "#{app_path}/log/unicorn.stdout.log"
-
- ↓↓↓↓↓↓↓ 以下のように変更 ↓↓↓↓↓↓
 
 # ../が一つ増えている
 app_path = File.expand_path('../../../', __FILE__)
